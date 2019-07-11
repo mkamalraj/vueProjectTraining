2435b2414fa38854d3c2e5c7e5a2b6c37db163e5
chandanrsc47 <saichandanrsc@gmail.com>
2019-07-11 09:37:44 +0530

    webdevelopment

--- src/components/sample2.vue
+++ src/components/sample2.vue
@@ -1,52 +1,13 @@
 <template>
 <div>
-<button v-on:click="counter++">increase</button>
-<button v-on:click="counter--">decrease</button>
-<p>counter:{{ counter }}</p>
-<p>result:{{ result() }}</p>
-<h2 v-html="finshedlink"></h2>
-<button v-on:mousemove="timer">timer</button>
-<p>{{hours}}:{{minutes}}:{{seconds}}</p>
-<p v-on:mousemove="updates">{{x}}/{{y}}-<span v-on:mousemove.stop="">stop</span></p>
-<input type="text" v-on:keyup.enter="alertbox">
+<h1>{{title}}</h1>
 </div>
 </template>
 <script>
 export default{
-	data(){
-	return{
-	counter:0,
-	hours:0,
-	minutes:0,
-	seconds:0,
-	x:0,
-	y:0,
-	finshedlink: '<a href="http://www.google.com">google</a>'
-    }
-	},
-	watch:{
-	updates:function(newValue,oldValue){
-	console.log(newValue,oldValue)
+	data({
+		return{
+			title:"vuejs project"
+		}
 	}
-	},
-
-	methods:{
-    result:function(){
-    return this.counter >5 ? 'greater 5':'smaller 5'
-    },
-	timer: function(){
-	this.hours++;
-	this.minutes+=60;
-	this.seconds+=360;
-	},
-	updates:function(event){
-	this.x=event.clientX;
-	this.y=event.clientY;
-	},
-	alertbox: function(){
-    alert("required strong text")
-	},
-	
 }
-}
-</script>
